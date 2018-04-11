e7705f515aa29455f9c25433df554a1ef5a681b9
Arrizal Amin <arrizalamin@gmail.com>
2018-04-11 20:55:45 +0700

    exclude async function test in node v6 and remove node v7 from travis

--- test/async-function.js
+++ test/async-function.js
@@ -2,19 +2,24 @@ const expect = require('expect.js')
 const functionReflector = require('../index')
 
 describe('Async Function', () => {
-  var fn = async function(a, b) {
-    return a;
-  }
+  before(function () {
+    if (process.version.startsWith('v6.')) {
+      this.skip()
+    }
+  })
 
   it('should have async to be true', () => {
+    var fn = async function(a, b) {
+      return a;
+    }
     const actual = functionReflector(fn).async
 
     expect(actual).to.be(true)
   })
 
   it('should have async to be false for non-async function', () => {
-      const actual = functionReflector(() => {}).async
+    const actual = functionReflector(() => {}).async
 
-      expect(actual).to.be(false)
+    expect(actual).to.be(false)
   })
 })
